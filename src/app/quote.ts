4ffa508f5759d3937a8f39f05afcbc53442edc88
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-15 08:41:24 +0300

    Creating class quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    name: string | any;
+    message: string | any;
+    author: string | any;
+    date: Date | any;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    { name: 'benard',author: 'Kobe Bryant',message: 'If you work hard enough, Dreams do come true',date: '5/5/2021'},
+    { name: 'jesse',author: 'Obama',message: 'Yes we can, Dreams do come true',date: '10/5/1965'},
+    { name: 'chepkwony',author: 'Martin Luther',message: 'I have a dream',date: '2/5/1890'},
+    
+ 
+ 
+   ]
 
   constructor() { }
 
