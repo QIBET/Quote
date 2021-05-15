1c7429fb51da54f717b68972d937e0efa754356b
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-15 17:33:59 +0300

    Linking parent and child forms

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from'@angular/core';
+import { Component, Output, EventEmitter, OnInit } from'@angular/core';
 import {Quote} from '../quote';
 
 @Component({
