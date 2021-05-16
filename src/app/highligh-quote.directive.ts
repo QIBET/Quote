998811c04d0ba64db4b8cb2eaa2e21ae81fc0926
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-16 09:45:15 +0300

    Creating the action part of the directive'

--- src/app/highligh-quote.directive.ts
+++ src/app/highligh-quote.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighQuote]'
 })
 export class HighlighQuoteDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+  @HostListener("click") onClicks(){
+    this.highlightQuote("teal")
+  }
+
+  private highlightQuote(action:string){
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
 
 }
